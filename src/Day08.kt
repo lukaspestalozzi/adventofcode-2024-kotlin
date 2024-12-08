5cba2d3849d024f611faa401a36da8439717f716
luBeast <lukas.pestalozzi@protonmail.com>
2024-12-08 10:54:32 +0100

    Solve Day8 Part2

--- src/Day08.kt
+++ src/Day08.kt
@@ -32,19 +32,49 @@ class Day08 : AbstractSolver("08", 14, 34) {
         }
     }
 
-    private data class Antinode(val frequency: Char, val row: Int, val col: Int)
+    private data class Antinode(val row: Int, val col: Int)
     private data class Antenna(val frequency: Char, val pos: Pos) {
 
-        fun antinode(other: Antenna): List<Antinode> {
+        fun antinodePart1(other: Antenna): List<Antinode> {
             if (other.frequency != frequency) {
                 return emptyList()
             }
 
             val diffRow = other.pos.row - pos.row
             val diffCol = other.pos.col - pos.col
-            val antinode1 = Antinode(frequency, other.pos.row + diffRow, other.pos.col + diffCol)
-            val antinode2 = Antinode(frequency, pos.row - diffRow, pos.col - diffCol)
+            val antinode1 = Antinode(other.pos.row + diffRow, other.pos.col + diffCol)
+            val antinode2 = Antinode(pos.row - diffRow, pos.col - diffCol)
             return listOf(antinode1, antinode2)
+        }
+
+        fun antinodePart2(other: Antenna, maxRow: Int, maxCol: Int): List<Antinode> {
+            if (other.frequency != frequency) {
+                return emptyList()
+            }
+            val antinodes = mutableListOf<Antinode>(Antinode(pos.row, pos.col), Antinode(other.pos.row, other.pos.col))
+
+            val diffRow = other.pos.row - pos.row
+            val diffCol = other.pos.col - pos.col
+            // direction this -> other
+            var mult = 1
+            do {
+                val antinode = Antinode(other.pos.row + mult * diffRow, other.pos.col + mult * diffCol)
+                antinodes.add(antinode)
+                mult++
+            } while (0 <= antinode.row && antinode.row <= maxRow //
+                && 0 <= antinode.col && antinode.col <= maxCol
+            )
+
+            // direction other -> this
+            mult = -1
+            do {
+                val antinode = Antinode(other.pos.row + mult * diffRow, other.pos.col + mult * diffCol)
+                antinodes.add(antinode)
+                mult--
+            } while (0 <= antinode.row && antinode.row <= maxRow //
+                && 0 <= antinode.col && antinode.col <= maxCol
+            )
+            return antinodes
         }
     }
 
@@ -76,29 +106,39 @@ class Day08 : AbstractSolver("08", 14, 34) {
     }
 
     override fun solvePart1(inputLines: List<String>): Number {
-        val input = createInput(inputLines, true)
+        val input = createInput(inputLines)
         val antennas = input.antennas
         val antinodes: MutableSet<Antinode> = mutableSetOf()
         for (pair in antennas.combinations(2)) {
             check(pair.count() == 2)
             val antenna: Antenna = pair.first()
-            antinodes.addAll(antenna.antinode(pair.last()))
+            antinodes.addAll(antenna.antinodePart1(pair.last()))
         }
-        logger.info { "antinodes: $antinodes" }
 
         // Check which antinodes are in the grid
         val inGrid = antinodes.filter {
             0 <= it.row && it.row <= input.maxRow //
                     && 0 <= it.col && it.col <= input.maxCol
         }
-        logger.info { "inGrid: $inGrid" }
-        return inGrid.map { Pos(it.row, it.col) }.toSet().count()
+        return inGrid.toSet().count()
     }
 
     override fun solvePart2(inputLines: List<String>): Number {
         val input = createInput(inputLines)
-        var solution: Long = 0
-        return solution
+        val antennas = input.antennas
+        val antinodes: MutableSet<Antinode> = mutableSetOf()
+        for (pair in antennas.combinations(2)) {
+            check(pair.count() == 2)
+            val antenna: Antenna = pair.first()
+            antinodes.addAll(antenna.antinodePart2(pair.last(), maxRow = input.maxRow, maxCol = input.maxCol))
+        }
+
+        // Check which antinodes are in the grid
+        val inGrid = antinodes.filter {
+            0 <= it.row && it.row <= input.maxRow //
+                    && 0 <= it.col && it.col <= input.maxCol
+        }
+        return inGrid.toSet().count()
     }
 }
 
